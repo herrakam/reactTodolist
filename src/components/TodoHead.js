24ef56b2b7c0b251355338045408eb6701cebf97
SeungHyun <fm10033@gmail.com>
2021-08-22 17:57:36 +0900

    TodoHead 날짜, 남은 할일 개수 구현

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -1,37 +1,47 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { useTodoState } from "./TodoContext";
 
 const TodoHeadBlock = styled.div`
-padding: 48px 32px 24px;
-border-bottom:1px solid #e9ecef;
-h1{
-    margin:0;
-    font-size:36px;
-    color:#343a40;
-}
-.day{
-    margin-top:4px;
-    color:#868e96;
-    font-size:21px;
-}
-.tasks-left{
-    color:#20c997;
-    font-size:18px;
-    margin-top:40px;
-    font-weight:bold;
-
-}
-`
+  padding: 48px 32px 24px;
+  border-bottom: 1px solid #e9ecef;
+  h1 {
+    margin: 0;
+    font-size: 36px;
+    color: #343a40;
+  }
+  .day {
+    margin-top: 4px;
+    color: #868e96;
+    font-size: 21px;
+  }
+  .tasks-left {
+    color: #20c997;
+    font-size: 18px;
+    margin-top: 40px;
+    font-weight: bold;
+  }
+`;
 
 function TodoHead() {
-    return (
-    <TodoHeadBlock>
-        <h1>2021.8.17</h1>
-        <div className="day">화요일</div>
-        <div className="tasks-left">할 일 2개 남음</div>
+  const today = new Date();
+  const date = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
+  const weekday = today.toLocaleDateString("ko-KR", { weekday: "long" });
 
+  const state = useTodoState();
+  const undoneTasks = state.filter((todo) => !todo.done);
+
+  return (
+    <TodoHeadBlock>
+      <h1>{date}</h1>
+      <div className="day">{weekday}</div>
+      <div className="tasks-left">할일 {undoneTasks.length}개 남음</div>
     </TodoHeadBlock>
-    )
+  );
 }
 
-export default TodoHead
+export default TodoHead;
